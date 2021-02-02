9bfa6cce8b2fc5a001369ecbb9b9c796ccb27f15
Tom Mears <tom.mears14@gmail.com>
2021-02-02 13:32:00 +0000

    typo

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const data = [
   {
     id: uuidv4(),
-    title: 'built using functional components',
+    title: 'built using function components',
     completed: false
   },
   {
